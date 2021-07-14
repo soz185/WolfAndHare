88a2456128ac18477278ce3f8f3bd30a05b721cb
Sozutova Olga <sozutova@list.ru>
2021-07-14 22:39:14 +0300

    Исправления.

--- src/wolfandhare/MainWindow.java
+++ src/wolfandhare/MainWindow.java
@@ -12,6 +12,9 @@ import org.eclipse.wb.swt.SWTResourceManager;
 import org.eclipse.swt.widgets.Button;
 import org.eclipse.swt.events.SelectionAdapter;
 import org.eclipse.swt.events.SelectionEvent;
+import org.eclipse.swt.layout.FormLayout;
+import org.eclipse.swt.layout.FormData;
+import org.eclipse.swt.layout.FormAttachment;
 
 public class MainWindow {
 
@@ -54,99 +57,100 @@ public class MainWindow {
 		shlWolfAndHare.setMinimumSize(new Point(840, 450));
 		shlWolfAndHare.setSize(840, 450);
 		shlWolfAndHare.setText("Wolf and Hare");
+		shlWolfAndHare.setLayout(null);
 		
 		Canvas canvas = new Canvas(shlWolfAndHare, SWT.NONE | SWT.DOUBLE_BUFFERED);
+		canvas.setBounds(10, 10, 433, 298);
 		canvas.setBackground(SWTResourceManager.getColor(SWT.COLOR_WHITE));
-		canvas.setBounds(10, 10, 500, 350);
 		
 		Label labelCountHares = new Label(shlWolfAndHare, SWT.RIGHT);
-		labelCountHares.setBounds(578, 26, 117, 15);
+		labelCountHares.setBounds(449, 27, 203, 22);
 		labelCountHares.setText("\u041A\u043E\u043B\u0438\u0447\u0435\u0441\u0442\u0432\u043E \u0437\u0430\u0439\u0446\u0435\u0432:");
 		
 		Spinner spinnerCountHare = new Spinner(shlWolfAndHare, SWT.BORDER);
+		spinnerCountHare.setBounds(658, 24, 64, 22);
 		spinnerCountHare.setMaximum(50);
-		spinnerCountHare.setBounds(701, 23, 47, 22);
 		
 		Spinner spinnerCountWolf = new Spinner(shlWolfAndHare, SWT.BORDER);
+		spinnerCountWolf.setBounds(658, 52, 64, 22);
 		spinnerCountWolf.setMaximum(35);
-		spinnerCountWolf.setBounds(701, 51, 47, 22);
 		
 		Spinner spinnerCountObstacle = new Spinner(shlWolfAndHare, SWT.BORDER);
+		spinnerCountObstacle.setBounds(658, 80, 64, 22);
 		spinnerCountObstacle.setMaximum(50);
-		spinnerCountObstacle.setBounds(701, 79, 47, 22);
 		
 		Label labelCountWolf = new Label(shlWolfAndHare, SWT.RIGHT);
-		labelCountWolf.setBounds(578, 54, 117, 15);
+		labelCountWolf.setBounds(449, 55, 203, 30);
 		labelCountWolf.setText("\u041A\u043E\u043B\u0438\u0447\u0435\u0441\u0442\u0432\u043E \u0432\u043E\u043B\u043A\u043E\u0432:");
 		
 		
 		Label labelCurrentCountWolf = new Label(shlWolfAndHare, SWT.NONE);
-		labelCurrentCountWolf.setBounds(10, 387, 117, 15);
+		labelCurrentCountWolf.setBounds(10, 352, 120, 22);
 		labelCurrentCountWolf.setText("\u041A\u043E\u043B\u0438\u0447\u0435\u0441\u0442\u0432\u043E \u0432\u043E\u043B\u043A\u043E\u0432: ");
 		
 		Label labelCurrentCountHare = new Label(shlWolfAndHare, SWT.NONE);
-		labelCurrentCountHare.setBounds(10, 366, 117, 15);
+		labelCurrentCountHare.setBounds(10, 324, 120, 22);
 		labelCurrentCountHare.setText("\u041A\u043E\u043B\u0438\u0447\u0435\u0441\u0442\u0432\u043E \u0437\u0430\u0439\u0446\u0435\u0432: ");
 		
 		Label labelVisionHare = new Label(shlWolfAndHare, SWT.NONE);
+		labelVisionHare.setBounds(449, 112, 203, 76);
 		labelVisionHare.setAlignment(SWT.RIGHT);
-		labelVisionHare.setBounds(539, 105, 156, 32);
 		labelVisionHare.setText("\u041C\u0430\u043A\u0441\u0438\u043C\u0430\u043B\u044C\u043D\u043E\u0435 \u0440\u0430\u0441\u0441\u0442\u043E\u044F\u043D\u0438\u0435, \u043D\u0430 \u043A\u043E\u0442\u043E\u0440\u043E\u043C \u0432\u0438\u0434\u044F\u0442 \u0437\u0430\u0439\u0446\u044B:");
 		
 		Label labelVisionWolf = new Label(shlWolfAndHare, SWT.NONE);
+		labelVisionWolf.setBounds(449, 152, 203, 70);
 		labelVisionWolf.setText("\u041C\u0430\u043A\u0441\u0438\u043C\u0430\u043B\u044C\u043D\u043E\u0435 \u0440\u0430\u0441\u0441\u0442\u043E\u044F\u043D\u0438\u0435, \u043D\u0430 \u043A\u043E\u0442\u043E\u0440\u043E\u043C \u0432\u0438\u0434\u044F\u0442 \u0432\u043E\u043B\u043A\u0438:");
 		labelVisionWolf.setAlignment(SWT.RIGHT);
-		labelVisionWolf.setBounds(539, 143, 156, 32);
 		
 		Spinner spinnerVisionHare = new Spinner(shlWolfAndHare, SWT.BORDER);
+		spinnerVisionHare.setBounds(658, 124, 64, 22);
 		spinnerVisionHare.setMaximum(40);
 		spinnerVisionHare.setMinimum(5);
-		spinnerVisionHare.setBounds(701, 109, 47, 22);
 		
 		Spinner spinnerVisionWolf = new Spinner(shlWolfAndHare, SWT.BORDER);
+		spinnerVisionWolf.setBounds(658, 166, 64, 22);
 		spinnerVisionWolf.setMaximum(60);
 		spinnerVisionWolf.setMinimum(5);
-		spinnerVisionWolf.setBounds(701, 143, 47, 22);
 		
 		Label currentCountHare = new Label(shlWolfAndHare, SWT.NONE);
+		currentCountHare.setBounds(136, 324, 55, 22);
 		currentCountHare.setText("0");
-		currentCountHare.setBounds(133, 366, 55, 15);
 		
 		Label currentCountWolf = new Label(shlWolfAndHare, SWT.NONE);
+		currentCountWolf.setBounds(136, 352, 55, 22);
 		currentCountWolf.setText("0");
-		currentCountWolf.setBounds(133, 387, 55, 15);
 		
 		Button buttonStep = new Button(shlWolfAndHare, SWT.NONE);
-		buttonStep.setBounds(578, 335, 75, 25);
+		buttonStep.setBounds(526, 335, 104, 25);
 		buttonStep.setText("\u0428\u0430\u0433");
 		
 		Button buttonStop = new Button(shlWolfAndHare, SWT.NONE);
-		buttonStop.setBounds(676, 335, 75, 25);
+		buttonStop.setBounds(653, 335, 104, 25);
 		buttonStop.setText("\u041E\u0441\u0442\u0430\u043D\u043E\u0432\u0438\u0442\u044C");
 		
 		Label labelCountObstacle = new Label(shlWolfAndHare, SWT.RIGHT);
+		labelCountObstacle.setBounds(449, 83, 203, 22);
 		labelCountObstacle.setText("\u041A\u043E\u043B\u0438\u0447\u0435\u0441\u0442\u0432\u043E \u043F\u0440\u0435\u043F\u044F\u0442\u0441\u0442\u0432\u0438\u0439:");
-		labelCountObstacle.setBounds(510, 82, 185, 15);
 		
 		Label lblNewLabel = new Label(shlWolfAndHare, SWT.NONE);
-		lblNewLabel.setBounds(754, 26, 55, 15);
+		lblNewLabel.setBounds(728, 27, 80, 22);
 		lblNewLabel.setText("(\u043C\u0430\u043A\u0441. 50)");
 		
 		Label lblNewLabel_1 = new Label(shlWolfAndHare, SWT.NONE);
+		lblNewLabel_1.setBounds(728, 55, 80, 30);
 		lblNewLabel_1.setText("(\u043C\u0430\u043A\u0441. 35)");
-		lblNewLabel_1.setBounds(754, 54, 55, 15);
 		
 		Label lblNewLabel_2 = new Label(shlWolfAndHare, SWT.NONE);
+		lblNewLabel_2.setBounds(728, 83, 80, 22);
 		lblNewLabel_2.setText("(\u043C\u0430\u043A\u0441. 50)");
-		lblNewLabel_2.setBounds(754, 82, 55, 15);
 		
 		Label lblNewLabel_3 = new Label(shlWolfAndHare, SWT.NONE);
+		lblNewLabel_3.setBounds(728, 127, 80, 27);
 		lblNewLabel_3.setText("(\u043C\u0430\u043A\u0441. 40)");
-		lblNewLabel_3.setBounds(754, 110, 55, 15);
 		
 		Label lblNewLabel_4 = new Label(shlWolfAndHare, SWT.NONE);
+		lblNewLabel_4.setBounds(728, 169, 80, 29);
 		lblNewLabel_4.setText("(\u043C\u0430\u043A\u0441. 60)");
-		lblNewLabel_4.setBounds(754, 146, 55, 15);
 		
 		// пошаговое выполнение
 		buttonStep.addSelectionListener(new SelectionAdapter() {
